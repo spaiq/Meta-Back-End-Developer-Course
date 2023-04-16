ce7f0b8478c2ebc1ab78544f7b43672f6026cf4b
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-04-16 17:55:34 +0200

    feat: add BookingViewSet

--- Back-End-Developer-Capstone/littlelemon/restaurant/views.py
+++ Back-End-Developer-Capstone/littlelemon/restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Menu, Booking
-from .serializers import MenuSerializer
+from .serializers import MenuSerializer, BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
@@ -13,3 +14,7 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
