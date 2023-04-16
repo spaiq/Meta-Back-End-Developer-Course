755a05214a475c1d6b33ff84905df54facf16234
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-04-16 16:46:28 +0200

    feat: add the actual Menu model

--- Back-End-Developer-Capstone/littlelemon/restaurant/models.py
+++ Back-End-Developer-Capstone/littlelemon/restaurant/models.py
@@ -4,4 +4,8 @@ class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     bookingDate = models.DateTimeField()
-    
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
