6c2a06d38eaf4c01c533512c84e8d00636df4595
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-04-16 16:52:33 +0200

    feat: add str override for Menu and Booking models

--- Back-End-Developer-Capstone/littlelemon/restaurant/models.py
+++ Back-End-Developer-Capstone/littlelemon/restaurant/models.py
@@ -4,8 +4,14 @@ class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     bookingDate = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return self.name + " " + str(self.bookingDate)
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.title + " " + str(self.price)
