9e0b73c8a67a486775e07e54f3443b2c378a9bf8
Bartłomiej Juszczyk <112722476+spaiq@users.noreply.github.com>
2023-04-16 18:52:46 +0200

    feat: set up djoser

--- Back-End-Developer-Capstone/littlelemon/littlelemon/settings.py
+++ Back-End-Developer-Capstone/littlelemon/littlelemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'restaurant',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {"USER_ID_FIELD": "username"}
